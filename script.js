ff2dd98d289909a50deac0bdede3ea28895ba663
AlisDimit <afalisa013@gmail.com>
2020-06-15 13:56:49 +0300

    files added

---
+++ script.js
@@ -0,0 +1,32 @@
+var rocketDistanceFromBottom = 0;
+var stepWidth = 1;
+
+function makeUpStep() {
+  var rocketElem = document.getElementById('rocket');
+  rocketDistanceFromBottom += stepWidth;
+  var bottomPx = rocketDistanceFromBottom + 'px';
+
+  rocketElem.style.bottom = bottomPx;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function restart() {
+  var rocketElem = document.getElementById('rocket');
+  rocketElem.style.bottom = "0px";
+  rocketDistanceFromBottom = 0;
+}
+
+async function startAnimation() {
+  var i = 0;
+  while (i < 500) {
+    i++;
+    await sleep(3);
+    if (i % 20 == 0) {
+      stepWidth += 1;
+    }
+    makeUpStep();
+  }
+}
